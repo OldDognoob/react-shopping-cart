72855b4767f7959966c8866aec68e31be7fc6b8a
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-20 15:12:09 +0200

    complete Step-3

--- src/App.js
+++ src/App.js
@@ -27,17 +27,12 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+			<Route 
+			exact 
+			path="/"
+			component={Products}
 			/>
-
+		
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
